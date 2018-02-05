49dc6ec98c01ca630d4a4a70489b86584a8b325c
Tom Nielsen <tanielsen@gmail.com>
2018-02-05 10:40:02 +0000

    update LTS; breaks

--- src/AdWords/Auth.hs
+++ src/AdWords/Auth.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE StandaloneDeriving #-}
 
-module AdWords.Auth 
+module AdWords.Auth
   {-( postRequest-}
   {-, reportUrlEncoded-}
   {-, credentials-}
@@ -17,6 +17,7 @@ import GHC.Generics (Generic)
 
 import Network.OAuth.OAuth2
 import Network.OAuth.OAuth2
+import Network.OAuth.OAuth2.TokenRequest (Errors(..))
 import Network.HTTP.Client.TLS
 import Network.HTTP.Client
 import Network.HTTP.Types.Header
@@ -33,15 +34,15 @@ tlsManager = newManager tlsManagerSettings
 
 type AdWords = RWST Credentials Text Customer IO
 
-postRequest :: 
+postRequest ::
      String
-  -> BS.ByteString 
+  -> BS.ByteString
   -> AdWords (Response BL.ByteString)
 postRequest url body = do
   req <- liftIO $ parseRequest url
   token <- _accessToken <$> get
   man <- liftIO tlsManager
-  let headers = 
+  let headers =
         [ (hUserAgent, "hs-adwords")
         , (hAuthorization, "Bearer " `BS.append` accessToken token)
         , (hContentType, "application/soap+xml")
@@ -51,12 +52,12 @@ postRequest url body = do
                   , requestBody = RequestBodyBS body
                   , method = "POST"
                   }
-  liftIO $ httpLbs req' man 
+  liftIO $ httpLbs req' man
 
 tshow :: Show a => a -> Text
 tshow = T.pack . show
 
-reportUrlEncoded :: 
+reportUrlEncoded ::
      String
   -> [(BS.ByteString, BS.ByteString)]
   -> AdWords (Response BL.ByteString)
@@ -64,9 +65,9 @@ reportUrlEncoded url body = do
   req <- liftIO $ parseRequest url
   token <- _accessToken <$> get
   man <- liftIO tlsManager
-  devToken <- developerToken <$> ask 
+  devToken <- developerToken <$> ask
   ccid <- _clientCustomerID <$> get
-  let headers = 
+  let headers =
         [ (hUserAgent, "hs-adwords")
         , (hAuthorization, "Bearer " `BS.append` accessToken token)
         , ("developerToken", BS.pack $ T.unpack devToken)
@@ -75,7 +76,7 @@ reportUrlEncoded url body = do
 
       req' = req  { requestHeaders = headers }
 
-  liftIO $ httpLbs (urlEncodedBody body req') man 
+  liftIO $ httpLbs (urlEncodedBody body req') man
 
 type ClientId = BS.ByteString
 type ClientSectret = BS.ByteString
@@ -96,44 +97,44 @@ data Credentials = Credentials {
 
 instance Binary Customer
 instance Binary Credentials
-instance Binary OAuth2 
-deriving instance Generic OAuth2 
+instance Binary OAuth2
+deriving instance Generic OAuth2
 
 -- refresh token is given only on first key exchange
 credentials :: MonadIO m =>
-     ClientId 
-  -> ClientSectret 
+     ClientId
+  -> ClientSectret
   -> DeveloperToken
   -> ClientCustomerId
   -> ExchangeKey
-  -> m (OAuth2Result (Credentials, Customer))
+  -> m (OAuth2Result Errors (Credentials, Customer))
 credentials cliendId clientSecret devToken ccid exchangeKey = liftIO $ do
-  let oa = OAuth2 
-        cliendId 
-        clientSecret 
-        authorizeEndpoint 
-        accessTokenEntpoint 
+  let oa = OAuth2
+        cliendId
+        clientSecret
+        authorizeEndpoint
+        accessTokenEntpoint
         (Just callback)
       accessTokenEntpoint = "https://www.googleapis.com/oauth2/v3/token"
       authorizeEndpoint = "https://accounts.google.com/o/oauth2/auth"
       callback = "urn:ietf:wg:oauth:2.0:oob"
 
-  man <- tlsManager 
+  man <- tlsManager
   res <- fetchAccessToken man oa exchangeKey
-  
+
   return $ fmap (\acc -> (Credentials oa devToken $ refreshToken acc, Customer ccid acc)) res
 
 deriving instance Generic AccessToken
 instance Binary AccessToken
 
 exchangeCodeUrl :: BS.ByteString -> BS.ByteString
-exchangeCodeUrl cId = 
-  "https://accounts.google.com/o/oauth2/auth?client_id=" 
-  <> cId 
+exchangeCodeUrl cId =
+  "https://accounts.google.com/o/oauth2/auth?client_id="
+  <> cId
   <> "&response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fadwords&redirect_uri=urn:ietf:wg:oauth:2.0:oob&access_type=offline&prompt=consent"
 
 refresh :: AdWords ()
-refresh = do 
+refresh = do
   mb_refToken <- refreshToken' <$> ask
   ccid <- _clientCustomerID <$> get
 
