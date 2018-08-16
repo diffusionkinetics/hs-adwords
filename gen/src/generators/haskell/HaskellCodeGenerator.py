c959e66dbc5ee9be3e0bbc9c27a0ae18886a9fce
Stefan Nožinić <stefan@lugons.org>
2018-08-16 18:28:03 +0200

    Generate FromXML class definition and move complex type code generation to another function

--- gen/src/generators/haskell/HaskellCodeGenerator.py
+++ gen/src/generators/haskell/HaskellCodeGenerator.py
@@ -23,17 +23,27 @@ class HaskellCodeGenerator(CodeGenerator):
             "long": "Int"
         }.get(t[t.find(":")+1:], t[t.find(":")+1:])
 
+    def complex_type(self, code, ct):
+        fields = {}
+        for sequence in ct.sequences:
+            for e in sequence.elements:
+                fields[e.name] = self.map_type(e.type)
+        code.type(ct.name, [(ct.name, fields)])
+        code("instance ToXML %s where" % ct.name)
+        code("  toXML e = element \"%s\" %s" % (ct.name, "do" if fields != {} else ""))
+        for k,v in fields.items():
+            code("    element \"%s\" $ content e.%s" % (k,k))
+        code("instance FromXML %s where" % ct.name)
+        code("  parseIt :: P.Document ")
+        code("  parseIt = case P.parseText def text of ")
+        code("    Left err -> P.parseText_ def \"<haha>lol</haha>\"")
+        code("    Right doc -> doc")
+
     def parse_schemas(self, parser):
         code = HaskellCodeBuilder()
+        code("class FromXML a where")
+        code("  parseIt :: Text -> a")
         for schema in parser.get_schemas():
             for ct in schema.complex_types:
-                fields = {}
-                for sequence in ct.sequences:
-                    for e in sequence.elements:
-                        fields[e.name] = self.map_type(e.type)
-                code.type(ct.name, [(ct.name, fields)])
-                code("instance ToXML %s where" % ct.name)
-                code("  toXML e = element \"%s\" %s" % (ct.name, "do" if fields != {} else ""))
-                for k,v in fields.items():
-                    code("    element \"%s\" $ content e.%s" % (k,k))
+                self.complex_type(code, ct)
         return str(code)
