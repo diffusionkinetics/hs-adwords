2a53623c8bc433be075f901bd5ccdb4a03570c19
Stefan Nožinić <stefan@lugons.org>
2018-08-16 18:03:32 +0200

    Update test

--- gen/test.py
+++ gen/test.py
@@ -1,4 +1,5 @@
 from src import *
+from src.generators.haskell import * 
 w = WSDLParser("adwords.xml")
 import lxml.etree as ET 
 
@@ -7,4 +8,8 @@ print(w.get_schemas())
 code = HaskellCodeBuilder()
 code.type("SomeType", [("A", {"x": "Int", "y": "[Char]"})])
 code.function("f", {"n": "Int"}, "Int", "n * ff (n-1)") 
-print(code.get_code())
+#print(code.get_code())
+
+generator = HaskellCodeGenerator()
+print(generator.parse_services(w))
+print(generator.parse_schemas(w))
